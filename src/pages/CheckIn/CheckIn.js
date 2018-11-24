6e28d364fbef03df18c1783326cddf558aa30237
hileix <304192604@qq.com>
2018-11-24 08:30:37 +0800

    fix(CheckIn.js): 修复组件 resid 错误的 bug

--- src/pages/CheckIn/CheckIn.js
+++ src/pages/CheckIn/CheckIn.js
@@ -8,7 +8,8 @@ class CheckIn extends React.Component {
     super(props);
 
     this.state = {
-      name: ''
+      name: '',
+      resid: 0
     };
   }
 
@@ -19,6 +20,7 @@ class CheckIn extends React.Component {
     const accessToken = params.get('accessToken');
     const theme = params.get('theme');
     const title = params.get('title');
+    const resid = params.get('resid');
 
     localStorage.setItem(
       'userInfo',
@@ -32,7 +34,7 @@ class CheckIn extends React.Component {
         '@primary-color': theme
       });
     }
-    this.setState({ name: title });
+    this.setState({ name: title, resid: parseInt(resid, 10) });
   };
 
   setThemeColor = themeColor => {
@@ -51,38 +53,40 @@ class CheckIn extends React.Component {
   };
 
   render() {
-    const resid = parseInt(this.props.match.params.resid, 10);
+    const { resid } = this.state;
     return (
       <div>
         <h1>{this.state.name}</h1>
-        <LzTable
-          resid={resid}
-          pagination={{ current: 0, pageSize: 10 }}
-          isSortBE
-          hasDownloadExcel
-          advSearchConfig={{
-            containerName: 'drawer', // 高级搜索所在容器的名字：'normal' 在表格里面；'drawer' 在抽屉容器里面
-            defaultVisible: false, // 默认是否显示
-            drawerWidth: 500, // 抽屉的宽度
-            labelWidth: '24%', // label 宽度
-            rowWidth: '100%', // row 宽度
-            dateRanges: [
-              // date
-              {
-                title: '考勤日期',
-                visible: [false, false, false, false], // 对应 “今天”、“昨天”、“本周”、“上周” 是否显示
-                innerFieldName: 'DayDate' // 内部字段
-              }
-            ],
-            search: [
-              // search
-              {
-                title: '工号/姓名',
-                innerFieldNames: ['C3_375380046640', 'C3_375380006609']
-              }
-            ]
-          }}
-        />
+        {resid && (
+          <LzTable
+            resid={resid}
+            pagination={{ current: 0, pageSize: 10 }}
+            isSortBE
+            hasDownloadExcel
+            advSearchConfig={{
+              containerName: 'drawer', // 高级搜索所在容器的名字：'normal' 在表格里面；'drawer' 在抽屉容器里面
+              defaultVisible: false, // 默认是否显示
+              drawerWidth: 500, // 抽屉的宽度
+              labelWidth: '24%', // label 宽度
+              rowWidth: '100%', // row 宽度
+              dateRanges: [
+                // date
+                {
+                  title: '考勤日期',
+                  visible: [false, false, false, false], // 对应 “今天”、“昨天”、“本周”、“上周” 是否显示
+                  innerFieldName: 'DayDate' // 内部字段
+                }
+              ],
+              search: [
+                // search
+                {
+                  title: '工号/姓名',
+                  innerFieldNames: ['C3_375380046640', 'C3_375380006609']
+                }
+              ]
+            }}
+          />
+        )}
       </div>
     );
   }
